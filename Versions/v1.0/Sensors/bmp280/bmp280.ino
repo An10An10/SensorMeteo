d8b77cff5887a983df81b88d62ab6c2f5dc1fe84
An10An10 <bgleb111@gmail.com>
2026-05-08 16:30:28 +0300

    Create bmp280.ino

---
+++ Versions/v1.0/Sensors/bmp280/bmp280.ino
@@ -0,0 +1,36 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <Wire.h>
+#include <Adafruit_BMP280.h>
+
+Adafruit_BMP280 bmp;
+
+WIFI_SSID = "WIFI_SSID"
+WIFI_PASS = "WIFI_PASS"
+ID = "1"
+
+void setup() {
+  Serial.begin(115200);
+  if (!bmp.begin(0x76)) { Serial.println("BMP280 error"); }
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) delay(500);
+}
+
+void loop() {
+  float t = bmp.readTemperature();
+  float p = (bmp.readPressure() / 100.0F) * 0.750062;
+
+  if (isnan(t)) t = -1.0;
+  if (p <= 0) p = -1.0;
+
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClient client;
+    HTTPClient http;
+    http.begin(client, "http://leoplng.ru:25580/update");
+    http.addHeader("id", ID);
+    String postData = String(t) + ",-1.0," + String(p);
+    http.POST(postData);
+    http.end();
+  }
+  delay(60000);
+}
